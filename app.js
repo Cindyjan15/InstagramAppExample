9906992f0684b8de6b20556f8b149473c507a284
Cindy Chen <Cindyjan15@gmail.com>
2014-04-09 22:37:56 -0700

    instgram App

--- app.js
+++ app.js
@@ -31,3 +31,6 @@ app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
