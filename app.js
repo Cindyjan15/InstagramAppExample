a0ac531b8d8beffa7e5db83c615aa3c08f702eed
Cindy Chen <Cindyjan15@gmail.com>
2014-04-09 22:50:41 -0700

    Instagram App

--- app.js
+++ app.js
@@ -4,32 +4,41 @@ var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
 var app = express();
-//route for hashtag
-var hashtag = require('./routes/hashtag');
-var index = require('./routes/index');
-//database setup
-var mongoose = require('mongoose');
-mongoose.connect(process.env.MONGOHQ_URL || 'mongodb://localhost/instagramexample');
+
+//load environment variables
+var dotenv = require('dotenv');
+dotenv.load();
+
+//add instagram api setup
+var ig = require('instagram-node-lib');
+ig.set('client_id', process.env.instagram_client_id);
+ig.set('client_secret', process.env.instagram_client_secret);
+
+ig.tags.info({
+	name: 'sushi',
+	complete: function(data) {
+		console.log(data);
+	}
+});
 
 //Configures the Template engine
 app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.bodyParser());
 
 //routes
-app.get('/', index.view);
+app.get('/', function (req, res) {
+	res.render('index');
+});
 app.get('/hashtag', function (req, res) {
 	res.render('hashtag');
 })
-app.post('/hashtag', hashtag.getHashtag);
-app.post('/save', hashtag.saveFavorites);
-app.post('/delete', index.deleteImage);
 //set environment ports and start application
 app.set('port', process.env.PORT || 3000);
 http.createServer(app).listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
+});port'));
 });
 //load environment variables
 var dotenv = require('dotenv');
